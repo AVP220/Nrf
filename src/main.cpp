e7b74e688f9d32275147460636881ed1671f66d0
AVP <avp220iv@gmail.com>
2024-11-15 23:10:31 +0200

    debag

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,42 @@
-#include <SPI.h>
+#include "nRF24L01.h" // библиотека радиомодуля
+#include <Arduino.h>
 #include <RF24.h>
+#include <SPI.h>
 #include <Servo.h>
-#include "nRF24L01.h"     // библиотека радиомодуля
-#include <Arduino.h> 
 
+#define V_T_BATTERY 512 // пока так потом через мап уже ридумаю
 
-#define V_T_BATTERY 512 //пока так потом через мап уже ридумаю
-
-Servo myServo;                  
+Servo myServo;
 RF24 radio(9, 10);
 
 const byte address[6] = "00001"; // ID приёмника
 bool statusBattery;
-bool statusFlag;           
-         
-
-
+bool statusFlag;
 
 void useFlag(int myCommand) {
   Serial.println(myCommand);
   switch (myCommand) {
-    case 1:
-      myServo.write(180);
-      statusFlag = 1;       
-      break;
+  case 1:
+    myServo.write(180);
+    statusFlag = 1;
+    break;
 
-    case 0:
-      myServo.write(0);  
-      statusFlag = 0;       
-      break;
+  case 0:
+    myServo.write(0);
+    statusFlag = 0;
+    break;
 
-    case 111:                    // Обработка запроса статуса
-      if(analogRead(0) <= V_T_BATTERY){
-        statusBattery = false;
-      }else{
-        statusBattery = true;
-      }
-      bool ToSend[2]{statusBattery, statusFlag};
-      radio.writeAckPayload(1, &ToSend, sizeof(ToSend)); 
-      break;
+  case 111: // Обработка запроса статуса
+    if (analogRead(0) <= V_T_BATTERY) {
+      statusBattery = false;
+    } else {
+      statusBattery = true;
+    }
+    bool ToSend[2]{statusBattery, statusFlag};
+    radio.writeAckPayload(1, &ToSend, sizeof(ToSend));
+    break;
   }
 }
-
-
 
 void setup() {
   Serial.begin(9600);
@@ -51,24 +45,22 @@ void setup() {
   radio.setPALevel(RF24_PA_LOW);
   radio.setDataRate(RF24_1MBPS);
   radio.openReadingPipe(1, address);
-  radio.startListening();  
-  radio.setChannel(0x60);  //выбираем канал (в котором нет шумов!)
-  radio.setAutoAck(1);         //режим подтверждения приёма, 1 вкл 0 выкл
-  radio.setRetries(0, 15);    //(время между попыткой достучаться, число попыток)
-  radio.setPayloadSize(6);     //размер пакета, в байтах
-  radio.powerUp(); //начать работу
-  radio.startListening();  //начинаем слушать эфир, мы приёмный модуль
+  radio.startListening();
+  radio.setChannel(0x60); // выбираем канал (в котором нет шумов!)
+  radio.setAutoAck(1); // режим подтверждения приёма, 1 вкл 0 выкл
+  radio.setRetries(0, 15); //(время между попыткой достучаться, число попыток)
+  radio.setPayloadSize(6); // размер пакета, в байтах
+  radio.powerUp();         // начать работу
+  radio.startListening(); // начинаем слушать эфир, мы приёмный модуль
 
-  myServo.attach(8);            
-
+  myServo.attach(8);
+  myServo.write(0);
 }
-
-
 
 void loop() {
   int myCommand;
   if (radio.available()) {
     radio.read(&myCommand, sizeof(myCommand));
-    useFlag(myCommand);         
+    useFlag(myCommand);
   }
 }
