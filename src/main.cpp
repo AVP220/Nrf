db2eee6f9b516c45f6e1f9461d3ff4257913d38e
AVP <avp220iv@gmail.com>
2024-11-19 23:45:28 +0200

    successful debag ping

--- src/main.cpp
+++ src/main.cpp
@@ -14,19 +14,21 @@ bool statusBattery;
 bool statusFlag;
 
 void useFlag(int myCommand) {
-  Serial.println(myCommand);
   switch (myCommand) {
   case 1:
+    Serial.println("Command 1");
     myServo.write(180);
     statusFlag = 1;
     break;
 
   case 0:
+  Serial.println("Command 0");
     myServo.write(0);
     statusFlag = 0;
     break;
 
   case 111: // Обработка запроса статуса
+  Serial.println("Command 111");
     if (analogRead(0) <= V_T_BATTERY) {
       statusBattery = false;
     } else {
@@ -42,14 +44,17 @@ void setup() {
   Serial.begin(9600);
 
   radio.begin();
+  
+  radio.setAutoAck(1); // режим подтверждения приёма, 1 вкл 0 выкл
+  radio.setRetries(0, 15); //(время между попыткой достучаться, число попыток)
+  radio.enableAckPayload(); // разрешить отсылку данных в ответ на входящий// сигнал
+  radio.setPayloadSize(6); // размер пакета, в байтах
+
+  radio.setChannel(0x60); // выбираем канал (в котором нет шумов!)
   radio.setPALevel(RF24_PA_LOW);
   radio.setDataRate(RF24_1MBPS);
   radio.openReadingPipe(1, address);
-  radio.startListening();
-  radio.setChannel(0x60); // выбираем канал (в котором нет шумов!)
-  radio.setAutoAck(1); // режим подтверждения приёма, 1 вкл 0 выкл
-  radio.setRetries(0, 15); //(время между попыткой достучаться, число попыток)
-  radio.setPayloadSize(6); // размер пакета, в байтах
+
   radio.powerUp();         // начать работу
   radio.startListening(); // начинаем слушать эфир, мы приёмный модуль
 
@@ -58,7 +63,8 @@ void setup() {
 }
 
 void loop() {
-  int myCommand;
+  int myCommand = -1;
+  Serial.println(myCommand);
   if (radio.available()) {
     radio.read(&myCommand, sizeof(myCommand));
     useFlag(myCommand);
